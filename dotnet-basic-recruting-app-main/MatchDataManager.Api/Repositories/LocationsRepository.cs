438f7f246da7e7b1e601919a7f6043229e5d1c9e
Mateusz Adam <mateusz.adam@fenergo.com>
2023-02-05 11:35:53 +0100

    changed the repository location/teams  classes to operate on db context for crud operations.

--- dotnet-basic-recruting-app-main/MatchDataManager.Api/Repositories/LocationsRepository.cs
+++ dotnet-basic-recruting-app-main/MatchDataManager.Api/Repositories/LocationsRepository.cs
@@ -1,105 +1,82 @@
 ﻿using MatchDataManager.Api.Models;
 using MatchDataManager.Api.Interfaces;
 using MatchDataManager.Api.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace MatchDataManager.Api.Repositories;
 
 
 public class LocationsRepository : ILocationsRepository
 {
-    private readonly MatchDataDbContext _matchDataDbContext;
+    private readonly MatchDataDbContext _context;
 
-    public LocationsRepository(MatchDataDbContext matchDataDbContext)
+    public LocationsRepository(MatchDataDbContext context)
     {
-        _matchDataDbContext = matchDataDbContext;
+        _context = context;
     }
-
-
 
     public void AddLocation(LocationModel location)
     {
-        ////sprawdzenie czy dane location o danej nazwie już istnieje i jesli nie to dodanie 
-        
-        //var locationAlreadyExists = _locations.FirstOrDefault(x => x.Name == location.Name);
+        // added check if location name isnt already there      
 
-        //if(locationAlreadyExists == null)
-        //{            
-        //    _locations.Add(location);
-        //}
-       
+        var existingLocation = _context.Locations.FirstOrDefault(x => x.Name == location.Name);
+        if (existingLocation is null)
+        {
+            var newLocation = new Location()
+            {
+                Name = location.Name,
+                City = location.City
+            };
+
+            _context.Locations.Add(newLocation);
+            _context.SaveChanges();
+        }
+
     }
 
     public void DeleteLocation(int locationId)
     {
-        //var location = _locations.FirstOrDefault(x => x.Id == locationId);
-        //if (location is not null)
-        //{
-        //    _locations.Remove(location);
-        //}
+        // thanks to linq no need to check if not null as previously, it executes only when condition is met.
+
+        _context.Locations.Where(x => x.Id == locationId).ExecuteDelete();
+        _context.SaveChanges();
     }
 
     public IEnumerable<LocationModel> GetAllLocations()
     {
-       // return _locations;
+        return _context.Locations.Select(location => new LocationModel()
+        {
+            Id = location.Id,
+            Name = location.Name,
+            City = location.City
+        }).ToList();
     }
 
     public LocationModel GetLocationById(int id)
     {
-      //  return _locations.FirstOrDefault(x => x.Id == id);
+        //projecting location from db to the location model class 
+
+        return _context.Locations.Where(x => x.Id == id)
+             .Select(location => new LocationModel()
+             {
+                 Id = location.Id,
+                 Name = location.Name,
+                 City = location.City
+             }).FirstOrDefault();
     }
 
     public void UpdateLocation(LocationModel location)
     {
-        //var existingLocation = _locations.FirstOrDefault(x => x.Id == location.Id);
-        //if (existingLocation is null || location is null)
-        //{
-        //    throw new ArgumentException("Location doesn't exist.", nameof(location));
-        //}
+        var existingLocation = _context.Locations.FirstOrDefault(x => x.Id == location.Id);
+        if (existingLocation is null)
+        {
+            throw new ArgumentException("Location doesn't exist.", nameof(location));
+        }
 
-        //existingLocation.City = location.City;
-        //existingLocation.Name = location.Name;
+        existingLocation.City = location.City;
+        existingLocation.Name = location.Name;
+
+        _context.Locations.Update(existingLocation);
+        _context.SaveChanges();
     }
 }
-
-
-//public static class LocationsRepository
-//{
-//    private static readonly List<Location> _locations = new();
-
-//    public static void AddLocation(Location location)
-//    {
-//        location.Id = Guid.NewGuid();
-//        _locations.Add(location);
-//    }
-
-//    public static void DeleteLocation(Guid locationId)
-//    {
-//        var location = _locations.FirstOrDefault(x => x.Id == locationId);
-//        if (location is not null)
-//        {
-//            _locations.Remove(location);
-//        }
-//    }
-
-//    public static IEnumerable<Location> GetAllLocations()
-//    {
-//        return _locations;
-//    }
-
-//    public static Location GetLocationById(Guid id)
-//    {
-//        return _locations.FirstOrDefault(x => x.Id == id);
-//    }
-
-//    public static void UpdateLocation(Location location)
-//    {
-//        var existingLocation = _locations.FirstOrDefault(x => x.Id == location.Id);
-//        if (existingLocation is null || location is null)
-//        {
-//            throw new ArgumentException("Location doesn't exist.", nameof(location));
-//        }
-
-//        existingLocation.City = location.City;
-//        existingLocation.Name = location.Name;
-//    }
-//}
--- dotnet-basic-recruting-app-main/MatchDataManager.Api/Repositories/TeamsRepository.cs
+++ dotnet-basic-recruting-app-main/MatchDataManager.Api/Repositories/TeamsRepository.cs
@@ -1,59 +1,75 @@
 ﻿using MatchDataManager.Api.Data;
 using MatchDataManager.Api.Interfaces;
 using MatchDataManager.Api.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace MatchDataManager.Api.Repositories;
 
 public class TeamsRepository : ITeamsRepository
 {
-    private readonly MatchDataDbContext _matchDataDbContext;
+    private readonly MatchDataDbContext _context;
     
-    TeamsRepository(MatchDataDbContext matchDataDbContext)
+    TeamsRepository(MatchDataDbContext context)
     {
-      _matchDataDbContext = matchDataDbContext;
+        _context = context;
     }
 
     public void AddTeam(TeamModel team)
     {
-        //var teamAlreadyExists = _teams.FirstOrDefault(x => x.Name == team.Name);
+        var existingTeam = _context.Teams.FirstOrDefault(x => x.Name == team.Name);
+        if (existingTeam is null)
+        {
+            var newTeam = new Team()
+            {
+                Name = team.Name,
+                CoachName = team.CoachName,
+            };
 
-        //if (teamAlreadyExists == null)
-        //{
-           
-        //    _teams.Add(team);
-        //}
-
+            _context.Teams.Add(newTeam);
+            _context.SaveChanges();
+        }
     }
 
     public void DeleteTeam(int teamId)
     {
-        //var team = _teams.FirstOrDefault(x => x.Id == teamId);
-        //if (team is not null)
-        //{
-        //    _teams.Remove(team);
-        //}
+        _context.Teams.Where(x => x.Id == teamId).ExecuteDelete();
+        _context.SaveChanges();
     }
 
     public IEnumerable<TeamModel> GetAllTeams()
     {
-       // return _teams;
+        return _context.Teams.Select(team => new TeamModel()
+        {
+            Id = team.Id,
+            Name = team.Name,
+            CoachName = team.CoachName
+        }).ToList();
     }
 
     public TeamModel GetTeamById(int id)
     {
-      //  return _teams.FirstOrDefault(x => x.Id == id);
+        return _context.Teams.Where(x => x.Id == id)
+             .Select(team => new TeamModel()
+             {
+                 Id = team.Id,
+                 Name = team.Name,
+                 CoachName = team.CoachName,
+             }).FirstOrDefault();
     }
 
     public void UpdateTeam(TeamModel team)
     {
-        //var existingTeam = _teams.FirstOrDefault(x => x.Id == team.Id);
-        //if (existingTeam is null || team is null)
-        //{
-        //    throw new ArgumentException("Team doesn't exist.", nameof(team));
-        //}
+        var existingTeam = _context.Teams.FirstOrDefault(x => x.Id == team.Id);
+        if (existingTeam is null)
+        {
+            throw new ArgumentException("Team doesn't exist.", nameof(team));
+        }
 
-        //existingTeam.CoachName = team.CoachName;
-        //existingTeam.Name = team.Name;
+        existingTeam.CoachName = team.CoachName;
+        existingTeam.Name = team.Name;
+
+        _context.Teams.Update(existingTeam);
+        _context.SaveChanges();
     }
 }
 
